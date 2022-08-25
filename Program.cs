4217380d2b006a3ca3424eddc87ca227fcb09755
Евгения <bakhaeva@gmail.com>
2022-08-25 23:17:01 +0300

    Task 13

--- Program.cs
+++ Program.cs
@@ -4,13 +4,14 @@ class MyNewClass
 {
     static void Main()
     {
-    // Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает 
-    // вторую цифру этого числа 
-        Console.WriteLine("Введите трехзначное число");
+
+        Console.WriteLine("Введите  число");
         int number= Convert.ToInt32(Console.ReadLine());
-        task10(number);
+        task13(number);
     }
 
+    // Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает 
+    // вторую цифру этого числа 
     public static void task10(int num)
     {
         if((num>=100 && num<1000) || (num>-1000 && num<=-100))
@@ -19,6 +20,18 @@ class MyNewClass
         }
         else   Console.WriteLine("Число не трехзначное");
     }
+    
+    //Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+    public static void task13(int num)
+    {
+
+        if(num.ToString().Length>=3)
+        {
+            System.Console.WriteLine($" Третья цифра {num.ToString()[2]}");
+        }
+        else   Console.WriteLine("Третьего числа нет");
+    }
+    
 }
 
 
