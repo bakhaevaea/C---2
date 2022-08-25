51c69fc5c045f274bb01733b7c4b939dcf799931
Евгения <bakhaeva@gmail.com>
2022-08-25 23:05:19 +0300

    Task 10

--- Program.cs
+++ Program.cs
@@ -4,8 +4,20 @@ class MyNewClass
 {
     static void Main()
     {
-    
-        Console.WriteLine("Hello, World");
+    // Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает 
+    // вторую цифру этого числа 
+        Console.WriteLine("Введите трехзначное число");
+        int number= Convert.ToInt32(Console.ReadLine());
+        task10(number);
+    }
+
+    public static void task10(int num)
+    {
+        if((num>=100 && num<1000) || (num>-1000 && num<=-100))
+        {
+            System.Console.WriteLine($"{(num % 100) / 10}");
+        }
+        else   Console.WriteLine("Число не трехзначное");
     }
 }
 
